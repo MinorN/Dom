b1387842be9457da52ae183c6ab674c06a08dcb8
MinorN <1481700559@qq.com>
2022-03-18 21:30:23 +0800

    完成before，插入到一个节点之前

--- src/dom.js
+++ src/dom.js
@@ -7,5 +7,8 @@ window.dom = {
     after(node,node2){
         node.parentNode.insertBefore(node2,node.nextSibling)
     },
+    before(node,node2){
+        node.parentNode.insertBefore(node2,node)
+    },
 }
 
